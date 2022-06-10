0e7b72a9bc4318b6c3a6363b8fa6e531ffac2ef6
NDALER <sdalvianto@protonmail.com>
2022-06-10 14:31:38 +0800

    ehe

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"monospace:size=10", "Noto Sans JP:size=12"};
+static const char *fonts[] = {"Firacode nerd font mono:size=10.5", "Noto Sans JP:size=10.5"};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *dynamic     = NULL;      /* -dy option; dynamic command to run on input change */
 static const char *colors[SchemeLast][2] = {
@@ -19,7 +19,7 @@ static unsigned int lines      = 0;
 static unsigned int columns    = 0;
 
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 21;
 static unsigned int min_lineheight = 8;
 
 /*
